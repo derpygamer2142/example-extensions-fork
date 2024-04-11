7da260eb8093818af9d0fe403d7b9097b4814354
derpygamer2142 <derpygamer2142@gmail.com>
2024-04-11 13:36:35 -0700

    it kinda work

--- extensions/derpygamer2142/test.js
+++ extensions/derpygamer2142/test.js
@@ -33,22 +33,132 @@
                         //     }
                         // }
                     },
+                    {
+                        opcode: "compileHat",
+                        blockType: Scratch.BlockType.EVENT,
+                        text: "uhhh idk",
+                        isEdgeActivated: false
+                    },
+                    {
+                        opcode: "compileStart",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "run the compiley thingy"
+                    },
+                    {
+                        opcode: "declareVar",
+                        blockType: Scratch.BlockType.COMMAND,
+                        text: "declare variable [NAME] as [VALUE]",
+                        arguments: {
+                            NAME: {
+                                type: Scratch.ArgumentType.STRING
+                            },
+                            VALUE: {
+                                type: Scratch.ArgumentType.STRING
+                            }
+                        }
+                    }
                 ]
             };
 
         }
 
+
+
+
+        // make a hat block and get all the blocks under the script?
         ablock(args, util) {
-            console.log(Scratch, util, vm)
+            //console.log(Scratch, util, vm)
             util.thread.tryCompile()
             console.log(util)
-            console.log()
-            // thread is located under util.thread.blockContainer._blocks ?
+            let script = util.thread
+            //console.log(script) // script[Object.getOwnPropertyNames(script)[0]].value.startingFunction
             return 1
         }
 
+        genInputTree(util,thread,blocks,check,addCheck) {
+            if (JSON.stringify(blocks[check].inputs) === JSON.stringify({})) {
+                return {
+                    block: blocks[check].opcode,
+                    id: blocks[check].id,
+                    isRaw: (blocks[check].fields != {})
+                }
+                // isRaw is whether it's a math_number or whatever
+            }
+            else {
+                let finalinputs = []
+                if (addCheck) {
+                    finalinputs.push({
+                        block: blocks[check].opcode,
+                        id: blocks[check].id,
+                        isRaw: (blocks[check].fields != {})
+                    }) 
+                }
+
+
+                let inputs = Object.getOwnPropertyNames(blocks[check].inputs)
+                finalinputs.push(inputs.length)
+                for (let i = 0; i < inputs.length; i++) {
+                    finalinputs.push(this.genInputTree(util, thread, blocks, blocks[check].inputs[inputs[i]].block,false))
+                }
+                return finalinputs
+            }
+
+
+        }
+
+        compile (util,thread,blocks,firstblock,addStart) {
+            let output = []
+            let held = firstblock
+            if (addStart) { output.push(blocks[held].opcode) }
+            let next = blocks[held].next
+            while (next != null) {
+                held = next
+                next = blocks[held].next
+                output.push(blocks[held].opcode)
+                let heldInputs = structuredClone(blocks[held].inputs)
+                
+                if (heldInputs.hasOwnProperty("SUBSTACK")) {
+                    delete heldInputs.SUBSTACK // this is a quick fix and probably won't play well with other extensions.
+                    // i will make a custom math/block system later
+                }
+                if (heldInputs.hasOwnProperty("SUBSTACK2")) {
+                    delete heldInputs.SUBSTACK2 // see previous comment
+                }
+                if (JSON.stringify(heldInputs) != JSON.stringify({})) {
+                    // if the block takes inputs excluding SUBSTACK and SUBSTACK2, generate an input tree for it
+                    output.push(Object.getOwnPropertyNames(heldInputs).length)
+                    for (let i = 0; i < Object.getOwnPropertyNames(heldInputs).length; i++) {
+                        output.push(this.genInputTree(util,thread,blocks,heldInputs[Object.getOwnPropertyNames(heldInputs)[i]].block,true))
+                    }
+                }
+                if (blocks[held].inputs.hasOwnProperty("SUBSTACK")) {
+                    output.push(this.compile(util,thread,blocks,blocks[held].inputs.SUBSTACK.block,true))
+                }
+                if (blocks[held].inputs.hasOwnProperty("SUBSTACK2")) {
+                    // support for n-number of branches is cringe and we don't need that kind of negativity in here
+                    // also no extensions in my pristine compiled hats
+                    if (!blocks[held].inputs.hasOwnProperty("SUBSTACK")) {
+                        output.push([])
+                    }
+                    output.push("else")
+                    output.push(this.compile(util,thread,blocks,blocks[held].inputs.SUBSTACK2.block,true))
+                }
+                
+            }
+            return output
+        }
+
+        compileStart (args,util) {
+            let threads = util.startHats("yourmother_compileHat") // this is cringe but idrc, it's temporary
+            threads.forEach((t) => {
+                t.tryCompile()
+            })
+            console.log(util)
+            console.log(this.compile(util,threads[0],threads[0].blockContainer._blocks,threads[0].topBlock,false))
+        }
+
+
         
     }
-
     Scratch.extensions.register(new DerpysExtension())
 })(Scratch);
