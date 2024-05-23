870530893bb40ad308374e3c5f8e9006786333f1
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2024-05-23 09:34:47 -0700

    fix some bugs, very basic compiler that only supports a couple of blocks

--- extensions/derpygamer2142/test.js
+++ extensions/derpygamer2142/test.js
@@ -83,8 +83,148 @@
 
         }
 
+        textFromOp(util, blob) {
+            // i can't remember if blocks is _blocks, so i'm just getting it again
+            const _blocks = util.thread.blockContainer._blocks
+            switch (blob.block) {
+                case "text": {
+                    return _blocks[blob.id].fields.TEXT.value
+                    break;
+                }
+                case "math_number": {
+                    return Scratch.Cast.toNumber(_blocks[blob.id].fields.NUM.value)
+                    break;
+                }
+            }
+        }
 
+    
+        genWGSL(util, blocks) {
+            let code = ""
+            for (let i = 0; i < blocks.length; i++) {
+                //console.log(code)
+                let b = blocks[i]
 
+                if (Array.isArray(b)) {
+                    code = code.concat(this.genWGSL(util, blocks[i]))
+                }
+                else {
+                    if (typeof b === "object") {
+                        const op = b.block
+                        switch (op) {
+                            case "operator_equals": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util, blocks[i+1]) : this.textFromOp(util, blocks[i+1]))
+                                code = code.concat(" === ") // temp
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util, blocks[i+2]) : this.textFromOp(util, blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_lt": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" < ") // temp
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_gt": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" > ") // temp
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_and": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" && ") // temp
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_or": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" || ") // temp
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_add": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" + ")
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_subtract": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" - ")
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_multiply": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" + ")
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            case "operator_divide": {
+                                code = code.concat(Array.isArray(blocks[i+1]) ? this.genWGSL(util,blocks[i+1]) : this.textFromOp(util,blocks[i+1]))
+                                code = code.concat(" + ")
+                                code = code.concat(Array.isArray(blocks[i+2]) ? this.genWGSL(util,blocks[i+2]) : this.textFromOp(util,blocks[i+2]))
+                                i += 2
+                                break;
+                            }
+
+                            default: {
+                                console.warn("invalid")
+                                return "Invalid operator!"
+                            }
+                        }
+                    }
+                    else {
+                        switch (b) {
+                            case "control_if": {
+                                code = code.concat("if (")
+                                code = code.concat(blocks[i+1].length > 0 ? this.genWGSL(util, blocks[i+1]) : "true")
+                                code = code.concat(") {\n")
+                                if (blocks[i+2].length > 0) {
+                                    code = code.concat(this.genWGSL(util, blocks[i+2]))
+                                }
+                                
+                                
+                                code = code.concat("\n}") // newlines for some semblance of readability
+                                i += 2
+                                console.log(code)
+                                break;
+                                // did i spell that right
+                                /*
+                                expected output:
+                                if (condition) {
+                                code
+                                }
+                                */
+                            }
+
+                            default: {
+                                console.warn("invalid")
+                                return "Invalid opcode!"
+                            }
+                        }
+                    }
+                }
+            }
+
+            return code
+        }
 
         // make a hat block and get all the blocks under the script?
         ablock(args, util) {
@@ -117,7 +257,7 @@
 
 
                 let inputs = Object.getOwnPropertyNames(blocks[check].inputs)
-                finalinputs.push(inputs.length)
+                //finalinputs.push(inputs.length)
                 for (let i = 0; i < inputs.length; i++) {
                     finalinputs.push(this.genInputTree(util, thread, blocks, blocks[check].inputs[inputs[i]].block,true))
                 }
@@ -129,6 +269,9 @@
 
         genBlock (util,thread,blocks,block) {
             let output = []
+            if (!blocks[block].hasOwnProperty("inputs")) {
+                return ""
+            }
             let heldInputs = structuredClone(blocks[block].inputs)
             output.push(blocks[block].opcode)
             if (heldInputs.hasOwnProperty("SUBSTACK")) {
@@ -140,7 +283,7 @@
             }
             if (JSON.stringify(heldInputs) != JSON.stringify({})) {
                 // if the block takes inputs excluding SUBSTACK and SUBSTACK2, generate an input tree for it
-                output.push(Object.getOwnPropertyNames(heldInputs).length)
+                //output.push(Object.getOwnPropertyNames(heldInputs).length)
                 for (let i = 0; i < Object.getOwnPropertyNames(heldInputs).length; i++) {
                     output.push(this.genInputTree(util,thread,blocks,heldInputs[Object.getOwnPropertyNames(heldInputs)[i]].block,true))
                 }
@@ -181,20 +324,32 @@
                 if (heldInputs.hasOwnProperty("SUBSTACK2")) {
                     delete heldInputs.SUBSTACK2 // see previous comment
                 }
-                if (JSON.stringify(heldInputs) != JSON.stringify({})) {
+                if (JSON.stringify(heldInputs) != JSON.stringify({}) || blocks[held].opcode === "control_if") {
                     // if the block takes inputs excluding SUBSTACK and SUBSTACK2, generate an input tree for it
-                    output.push(Object.getOwnPropertyNames(heldInputs).length)
-                    for (let i = 0; i < Object.getOwnPropertyNames(heldInputs).length; i++) {
-                        output.push(this.genInputTree(util,thread,blocks,heldInputs[Object.getOwnPropertyNames(heldInputs)[i]].block,true))
+                    //output.push(Object.getOwnPropertyNames(heldInputs).length)
+                    if (Object.getOwnPropertyNames(heldInputs).length === 0) {
+                        output.push([])
+                    }
+                    else {
+                        for (let i = 0; i < Object.getOwnPropertyNames(heldInputs).length; i++) {
+                            output.push(this.genInputTree(util,thread,blocks,heldInputs[Object.getOwnPropertyNames(heldInputs)[i]].block,true))
+                        }
                     }
                 }
                 else {
                     console.log(JSON.stringify(heldInputs) + " does not require a tree")
                 }
-                if (blocks[held].inputs.hasOwnProperty("SUBSTACK")) {
-                    output.push(this.compile(util,thread,blocks,blocks[held].inputs.SUBSTACK.block,true))
+                if (blocks[held].inputs.hasOwnProperty("SUBSTACK") || blocks[held].opcode === "control_if") {
+                    if (blocks[held].opcode === "control_if" && !blocks[held].inputs.hasOwnProperty("SUBSTACK") && !blocks[held].inputs.hasOwnProperty("SUBSTACK2")) {
+                        output.push([])
+                        console.log("added []")
+                    }
+                    else {
+                        output.push(this.compile(util,thread,blocks,blocks[held].inputs.SUBSTACK.block,true))
+                    }
+                    
                 }
-                if (blocks[held].inputs.hasOwnProperty("SUBSTACK2")) {
+                if (blocks[held].inputs.hasOwnProperty("SUBSTACK2")) { // remember to check if it's an empty if-else
                     // support for n-number of branches is cringe and we don't need that kind of negativity in here
                     // also no extensions in my pristine compiled hats
                     if (!blocks[held].inputs.hasOwnProperty("SUBSTACK")) {
@@ -214,7 +369,9 @@
                 t.tryCompile()
             })
             console.log(util)
-            console.log(this.compile(util,threads[0],threads[0].blockContainer._blocks,threads[0].topBlock,false))
+            const e = this.compile(util,threads[0],threads[0].blockContainer._blocks,threads[0].topBlock,false)
+            console.log(e)
+            console.log(this.genWGSL(util, e))
         }
 
         declareVar (args, util) {
